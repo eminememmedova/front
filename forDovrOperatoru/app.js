9b07e48a7c488a3342d14d7106d3b50e8e550b2a
emine <memmedovaemine553@gmail.com>
2023-12-29 17:34:38 +0400

    29.12

---
+++ forDovrOperatoru/app.js
@@ -0,0 +1,36 @@
+//?For dongusu
+
+// for(let i=1; i<5; i++){
+//     console.log(i);
+// }
+
+//?1den 10a qeder cut ededleri yazdir
+
+// for(i=0; i<=10; i+=2){
+//     console.log(i);
+// } 
+
+//?1den 10a qeder teklerin yazilmasi
+
+// for(i=1; i<=10; i+=2){
+//     console.log(i);
+// }
+
+//?1den 10a qeder tek ve cut edeleri gostersin
+
+// for(i=1; i<=10; i++){
+//     if (i%2==0) {
+//         console.log(i+": " + "cut");
+//     }else{
+//         console.log(i+": " + "tek");
+//     }
+// }
+
+//?50den geri 1e qeder sayir ve butun ededlerin cemini tapir
+
+// let cem = 0;
+// for (let i = 50; i >= 1; i--) {
+//     cem += i;
+//     console.log(i);
+// }
+// console.log("cem: " + cem);
---
+++ whileDovrOperatoru/app.js
@@ -0,0 +1,30 @@
+//? While Dovr operatoru
+
+
+//? a 10dan kicik beraber olana qeder console'da yaz ve 1vahid artir
+// let a = 5;
+// while (a<=10) {
+//     console.log(a);
+//     a++;
+// }
+
+//? 1den ona qeder cut ve tek ededleri goster
+// let a = 1;
+// while (a <= 10) {
+//     if (a % 2 == 0) {
+//         console.log(a + ": cut");
+//     } else {
+//         console.log(a + ": tek");
+//     }
+//     a++;
+// }
+
+// ? a 7 olanadek dovre sal ve console'a yazdir
+// let a=1;
+// while (true) {
+//     console.log(a);
+//     if (a==7) {
+//         break;
+//     }
+//     a++;
+// }
